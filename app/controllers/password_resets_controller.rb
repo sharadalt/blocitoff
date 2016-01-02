5e12451aa9e00eeb2ce6c0e9815a7f088961d9cd
sharadalt <ltsharada@gmail.com>
2016-01-02 05:09:51 +0000

    user_sign_and_out

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -16,7 +16,7 @@ class PasswordResetsController < ApplicationController
     if @user.password_reset_sent_at < 2.hours.ago
       redirect_to new_password_reset_path, :alert => "Password reset has expired."
     elsif @user.update_attributes(params.require(:user).permit(:password, :password_confirmation))
-      redirect_to new_password_reset_path, :alert => "Password has been reset."
+      redirect_to root_path, :alert => "Password has been reset."
     else 
       render edit
     end
